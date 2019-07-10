f55b4def27d0fb54369dde6b2f1fef33a333145f
yu <229513407@qq.com>
2019-07-10 20:57:29 +0800

    1.权限控制多余按钮去除

--- src/assets/lib/powerControl.js
+++ src/assets/lib/powerControl.js
@@ -23,88 +23,21 @@ export const powerControlLib = function (level, route) {
             };
             switch (level) {
                 case X:
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
-                    buttonControl.operatorAssignShow = false;//指派执行人
                     buttonControl.exportDataShow = true;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
-                    break;
-                case B: //基地经理
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
-                    buttonControl.operatorAssignShow = false;//指派执行人
-                    buttonControl.exportDataShow = false;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
-                    break;
-                case E: //访销员
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
-                    buttonControl.operatorAssignShow = false;//指派执行人
-                    buttonControl.exportDataShow = false;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
-                    break;
-                case WE: //战区执行人
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
-                    buttonControl.operatorAssignShow = false;//指派执行人
-                    buttonControl.exportDataShow = false;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
-                    break;
-                case W: // 战区经理
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
-                    buttonControl.operatorAssignShow = false;//指派执行人
-                    buttonControl.exportDataShow = false;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
                     break;
                 case WF: // 战区负责人
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
                     buttonControl.operatorAssignShow = true;//指派执行人
-                    buttonControl.exportDataShow = false;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
                     break;
                 case D: //督导组长
                     buttonControl.warAssignShow = true;//战区分派
                     buttonControl.superviseAssignShow = true;//督导分派
-                    buttonControl.operatorAssignShow = false;//指派执行人
                     buttonControl.exportDataShow = true;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
-                    break;
-                case DE: //督导组员
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
-                    buttonControl.operatorAssignShow = false;//指派执行人
-                    buttonControl.exportDataShow = false;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
                     break;
                 case K: //KA组长
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
                     buttonControl.operatorAssignShow = true;//指派执行人
-                    buttonControl.exportDataShow = false;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
-                    break;
-                case KE: //KA组员
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
-                    buttonControl.operatorAssignShow = false;//指派执行人
-                    buttonControl.exportDataShow = false;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
                     break;
                 case T: //电商组长
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
                     buttonControl.operatorAssignShow = true;//指派执行人
-                    buttonControl.exportDataShow = false;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
-                    break;
-                case TE: //电商组员
-                    buttonControl.warAssignShow = false;//战区分派
-                    buttonControl.superviseAssignShow = false;//督导分派
-                    buttonControl.operatorAssignShow = false;//指派执行人
-                    buttonControl.exportDataShow = false;//导出数据
-                    buttonControl.changeOperatorShow = false;//调整执行人
                     break;
                 default :
                     break;
